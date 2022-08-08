d1ab0cfca40821e00502a3080d1722877b969306
HyeonWoo Park <78792358+smartcow99@users.noreply.github.com>
2022-08-08 15:35:15 +0900

    [Silver III] Title: 바이러스, Time: 132 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,7 +10,6 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int C = Integer.parseInt(br.readLine());
-
 
         int graph[][] = new int[N+1][N+1];
         boolean flags[] = new boolean[N+1];
