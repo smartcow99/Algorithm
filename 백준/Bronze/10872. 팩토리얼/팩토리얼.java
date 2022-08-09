4c91bd7e268a5249ede2063acea63fa6019767d7
HyeonWoo Park <78792358+smartcow99@users.noreply.github.com>
2022-08-09 13:54:52 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int result = 1;
+        if (N != 0) {
+            for (int i = 1; i < N + 1; i++) {
+                result *= i;
+            }
+        }
+        System.out.println(result);
+    }
+}
