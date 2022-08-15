0a5c2ab516e30e831431596a207bcf1c0b38f361
HyeonWoo Park <78792358+smartcow99@users.noreply.github.com>
2022-08-15 17:59:22 +0900

    [Silver V] Title: 뒤집기, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st0 = new StringTokenizer(str, "0");
+        StringTokenizer st1 = new StringTokenizer(str, "1");
+        System.out.println(Math.min(st0.countTokens(), st1.countTokens()));
+    }
+}
+
