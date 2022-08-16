8fb12f3df9cd3fc528f3d1e699c221cd63b86acf
HyeonWoo Park <78792358+smartcow99@users.noreply.github.com>
2022-08-16 11:48:30 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -4,39 +4,50 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int cnt = 0;
-    public static void main(String args[]) throws IOException {
+    static int N, M;
+    static int[] parents;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-        int C = Integer.parseInt(br.readLine());
+        parents = new int[N+1];
+        for(int i=1; i<N+1; i++) {
+            parents[i] = i;
+        }
 
-        int graph[][] = new int[N+1][N+1];
-        boolean flags[] = new boolean[N+1];
-
-        for(int i=0; i<C; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
+        while(M-->0) {
+            st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            graph[x][y] = 1;
-            graph[y][x] = 1;
+            union(x, y);
         }
-        dfs(1, flags, graph);
-        System.out.println(cnt-1);
-    }
-    static void dfs(int v, boolean[] flags, int[][] graph) {
-        if(flags[v] == true) return;
 
-        flags[v] = true;
-        cnt++;
+        int cnt = 0;
+        int root = find(1);
 
-        for(int i=0; i<graph[v].length; i++) {
-            if(graph[v][i] == 1 && !flags[i]) {
-                dfs(i, flags, graph);
-            }
+        for(int i=2; i<N+1; i++) {
+            if(find(i) == root)
+                cnt++;
         }
+        System.out.println(cnt);
     }
+    static void union(int x, int y) {
+        x =find(x);
+        y = find(y);
 
+        if(x != y)
+            parents[y] = x;
+    }
+    static int find(int x) {
+        if(x == parents[x])
+            return x;
+
+        int root = find(parents[x]);
+        parents[x] = root;
+        return root;
+    }
 }
+
