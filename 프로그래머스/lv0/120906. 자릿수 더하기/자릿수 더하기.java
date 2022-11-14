1623c95cb86c3c85baedf0c8172144e12e7a1fa8
HyeonWoo Park <78792358+smartcow99@users.noreply.github.com>
2022-11-14 10:23:50 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 65.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String s = Integer.toString(n);
+        for(int i=0; i<s.length(); i++) 
+            answer += s.charAt(i) - '0';
+        
+        return answer;
+    }
+}
