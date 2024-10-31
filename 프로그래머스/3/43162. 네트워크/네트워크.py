094b108f271edaf9480aaa4d51d4ef836855b306
HyeonWoo Park <78792358+smartcow99@users.noreply.github.com>
2024-10-31 22:17:16 +0900

    [level 3] Title: 네트워크, Time: 0.78 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,19 @@
 from collections import deque
 
-def bfs(graph, start, visit):
-    que = deque([start])
-    while que:
-        v = que.popleft()
-        for i in range(len(graph)):
-            if graph[v][i] == 1 and not visit[i]:
-                visit[i] = True
-                que.append(i)
-
 def solution(n, computers):
-    cnt = 0
-    visit = [False] * n
+    answer = 0
+    visited = [0 for _ in range(n)]
+    deq = deque()
     for i in range(n):
-        if not visit[i]:
-            bfs(computers, i, visit)
-            cnt += 1
-    return cnt
+        if visited[i] == 1:
+            continue
+        deq.append(i)
+        answer += 1
+        while deq:
+            target = deq.popleft()
+            for j in range(n):
+                if computers[target][j] == 1 and visited[j] == 0:
+                    visited[j] = 1
+                    deq.append(j)
+                    
+    return answer
