038b5c89dd988656b0203733343c355a7e7e7a45
HyeonWoo Park <78792358+smartcow99@users.noreply.github.com>
2025-02-25 16:23:27 +0900

    [Bronze II] Title: 게임말 올려놓기, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/31995. 게임말 올려놓기/게임말 올려놓기.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int M = Integer.parseInt(br.readLine());
+    int result = (N - 1) * (M - 1) * 2;
+    System.out.println(result);
+  }
+}
